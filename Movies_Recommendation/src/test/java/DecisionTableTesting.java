8d4108324a33a9c6b98cc49f2559a8e275934402
AmgadShrief <amgadshrief273@gmail.com>
2025-05-08 00:23:54 +0300

    adding the decision table test functions

--- Movies_Recommendation/src/test/java/DecisionTableTesting.java
+++ Movies_Recommendation/src/test/java/DecisionTableTesting.java
@@ -29,4 +29,19 @@ public class DecisionTableTesting {
         String[] actual=movieManager.movieData.toArray(new String[0]);
         Assert.assertArrayEquals("movies mismatch", expected, actual);
     }
+    public void UserIdTest(){
+        String[] expected={
+                "ERROR: User Id 1234567A is wrong"
+        };
+        String[] actual=movieManager.movieData.toArray(new String[0]);
+        Assert.assertArrayEquals("Error in User ID", expected, actual);
+    }
+
+    public void MovieIdTest(){
+        String[] expected={
+                "ERROR: Movie Id digits in TM23 must be exactly 3"
+        };
+        String[] actual=movieManager.movieData.toArray(new String[0]);
+        Assert.assertArrayEquals("Error in Movie ID", expected, actual);
+    }
 }
